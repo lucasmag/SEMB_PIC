f1e5b7b45f1f90165ca94ac6902b5d30bc984178
Tiago Siqueira Dionizio <tiagosdionizio@gmail.com>
2018-11-16 21:34:01 -0300

    Corrigindo arquivo

--- ScriptSerial.py
+++ ScriptSerial.py
@@ -1,50 +1,43 @@
 import serial
 from serial import Serial
+import array
 
-ser = serial.Serial(port = "/dev/ttyUSB0", baudrate = 9600, timeout = 0)
-
-nPixels = 0
-pixel = ser.read(1)
-pixels = []
-ser.write(b'ROB$')
-#ser.write(b'GET$')
-
-totalPixelsEsperado = 3010
-print("Recebendo pixels")
-while nPixels < totalPixelsEsperado:
+def recebe_pixels():
+    nPixels = 0
     pixel = ser.read(1)
-    if(pixel != b''):
-        nPixels += 1
-        pixels.append(ord(pixel))
+    pixels = []
+    totalPixelsEsperado = 3010
+    print("Recebendo pixels")
+    
+    while nPixels < totalPixelsEsperado:
+        pixel = ser.read(1)
+        if(pixel != b''):
+            nPixels += 1
+            pixels.append(ord(pixel))
+    print("Transferência concluída!")
+    return pixels
 
-print("Transferência concluída!")
-print("Gerando imagem RobertPIC.PGM")
-imagemFile = open("RobertPIC.PGM","w")
-imagemFile.write("P2\n70 43\n255\n")
+def cria_p5(pixels):
+    print("Gerando imagem RobertPIC.PGM")
+    img = array.array('B')
+    for i in range(70):
+        for j in range(43):
+            if(j != 43):
+                img.append(pixels[i* 43 + j])
+            else:
+                img.append(pixels[i* 43 + j])
+    return img
 
-#pixelsBytes = bytearray(pixels)
-#imagemFile = open("RobertPIC.PGM","wb+")
-for i in range(70):
-    for j in range(43):
-        if(j != 43):
-            imagemFile.write("%d " % pixels[i* 43 + j])
-        else:
-            imagemFile.write("%d" % pixels[i* 43 + j])
-    imagemFile.write("\n")
-imagemFile.write("\r")
-imagemFile.close()
-'''
-imagemFile = open("RobertPIC.PGM","ab+")
-imagemFile.write(pixelsBytes)
-imagemFile = open("RobertPIC.PGM","a+")
-imagemFile.write("\r")
-'''
-print("Imagem gerada com sucesso!")
-imagemFile.close()
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    ser = serial.Serial(port = "/dev/ttyUSB0", baudrate = 9600, timeout = 0)
+    ser.write(b'ROB$')
+    #ser.write(b'GET$')
+    pixels = recebe_pixels()
+    img = cria_p5(pixels)
+    imageFile = open("RobertPIC.PGM","w")
+    imageFile.write("P5\n70 43\n255\n")
+    
+    imageFile = open("RobertPIC.PGM","ab")
+    img.tofile(imageFile)
+    print("Imagem .pgm P5 gerada com sucesso!")
+    imageFile.close()
