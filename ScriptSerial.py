ad17a367b16cf7fe437d96dc39e6e3730b9d4ba3
Lucas <lucasmag97@gmail.com>
2018-11-21 03:10:24 -0300

    Melhorias no código e adição de comentários

--- ScriptSerial.py
+++ ScriptSerial.py
@@ -1,7 +1,25 @@
+/*
+ *  Este script é responsável pela recepção dos bytes da imagem gerados pela MCU, logo após a aplicação do filtro. 
+ *  A medida em que os bytes são recebidos, eles são guardados em um array. Quando a imagem estiver completa, o script gera
+ *  um arquivo .PGM P5, contendo a imagem já pronta para teste.
+
+ *  Autores : Tiago Dionizio e Lucas Magalhães
+
+ *  Copyright (C) 2018 Tiago Siqueira Dionizio  <tiagosdionizio@gmail.com>
+
+ *  Copyright (C) 2018 Lucas Magalhães de Sousa <lucasmag97@gmail.com> 
+
+ *  Data de Atualização : 21 de Novembro de 2018
+*/
+
 import serial
 from serial import Serial
 import array
 
+/*
+ * Esta função define que a recepção será feita byte a byte, define o número total de pixels a serem recebidos e inicia a espera
+ * dos bytes. Quando todos os bytes forem recebidos é retornado um array contendo a imagem.
+ */
 def recebe_pixels():
     nPixels = 0
     pixel = ser.read(1)
@@ -16,22 +34,24 @@ def recebe_pixels():
             pixels.append(ord(pixel))
     print("Transferência concluída!")
     return pixels
-
+/*
+ * Esta função transfroma o array em um bytearray
+ */
 def cria_p5(pixels):
     print("Gerando imagem RobertPIC.PGM")
     img = array.array('B')
-    for i in range(70):
-        for j in range(43):
-            if(j != 43):
-                img.append(pixels[i* 43 + j])
-            else:
-                img.append(pixels[i* 43 + j])
+    for i in range(3010):
+        img.append(pixels[i])
     return img
 
+/*
+ * Quando o script é executado são definidas a porta de recepção e a taxa de transmissão. O comando ROB é enviado à MCU, para que se inicie
+ * a execução do filtro e logo em seguida é iniciada a espera dos pixels convertidos. Depois a função cria um arquivo .PGM, escreve o 
+ * cabeçalho e os bytes do bytearray, e por fim gera como saída a imagem .PGM com o filtro já aplicado.
+ */
 if __name__ == "__main__":
     ser = serial.Serial(port = "/dev/ttyUSB0", baudrate = 9600, timeout = 0)
     ser.write(b'ROB$')
-    #ser.write(b'GET$')
     pixels = recebe_pixels()
     img = cria_p5(pixels)
     imageFile = open("RobertPIC.PGM","w")
@@ -40,4 +60,4 @@ if __name__ == "__main__":
     imageFile = open("RobertPIC.PGM","ab")
     img.tofile(imageFile)
     print("Imagem .pgm P5 gerada com sucesso!")
-    imageFile.close()
+imageFile.close()
