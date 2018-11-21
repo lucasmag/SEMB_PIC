a2f15622e5fd9e5779825c209603807ee9255c64
Tiago Siqueira Dionizio <tiagosdionizio@gmail.com>
2018-11-20 22:14:44 -0300

    Código finalizado e comentado

--- main.c
+++ main.c
@@ -1,80 +1,126 @@
-/* 
- * File:   main.c
- * Author: Tiago Dionizio
- *
- * Created on 11 de Novembro de 2018, 14:22
- */
+/*
+ *  Sobre este código:
+ *  O arquivo main.c configura os registradores e bits de configuração do pic 16f18875 e aqui encontra-se a rotina 
+ *  principal da aplicação. O MCU fica a espera de um comando enviado pela porta serial, e executa o algorito de
+ *  Roberts Cross para detecção de bordas na imagem armazenada no arquivo image.h. Os valores dos novos pixels são
+ *  enviados para a interface serial onde podem ser lidos por um pc através de um adaptador usb/serial(ttl).
+
+ *  Autores : Tiago Dionizio e Lucas Magalhães
+
+ *  Copyright (C) 2018 Tiago Siqueira Dionizio <tiagosdionizio@gmail.com>
+
+ *  Copyright (C) 2018 Lucas Magalhães  <lucasmag97@gmail.com> 
+
+ *  Data de Atualização : 20 de Novembro de 2018
+
+ *  Modo de uso:
+ *  A aplicação é controlada por comandos enviados para a entrada serial EUSART (RX - RC7, TX - RC6) do MCU, essas 
+ *  entradas podem ser enviadas por um script em python usando o módulo pySerial, por meio desse script é possível
+ *  ordenar a execução do algoritmo de Roberts e gerar a imagem resultante com os pixels calculados e enviados pelo
+ *  MCU. 
+
+ *  Restrição de uso:
+ *  Necessita-se de um interpretador python ( >= 2.7) instalado na máquina que se comunicará com a aplicação,
+ *  bem como o módulo pySerial. Além disso, é necessário um adaptador usb/serial(ttl), como o pl2303, de modo que 
+ *  que possa haver um canal de comunicação entre o computador e a aplicação embarcada. A imagem gerada é em formato
+ *  .pgm binário (P5).
+
+ *  Referência para o algoritmo: 
+ *  MARQUES FILHO, Ogê; VIEIRA NETO, Hugo. Processamento Digital de Imagens, Rio de Janeiro: Brasport, 1999, pg: 97.
+*/
+
 
 // Production -> Set Configuration Bits;
 // PIC16F18875 Configuration Bit Settings
 // 'C' source line config statements
 
 // CONFIG1
-#pragma config FEXTOSC = OFF    // External Oscillator mode selection bits (Oscillator not enabled)
-#pragma config RSTOSC = HFINT32 // Power-up default value for COSC bits (HFINTOSC with OSCFRQ= 32 MHz and CDIV = 1:1)
-#pragma config CLKOUTEN = OFF   // Clock Out Enable bit (CLKOUT function is disabled; i/o or oscillator function on OSC2)
-#pragma config CSWEN = ON       // Clock Switch Enable bit (Writing to NOSC and NDIV is allowed)
-#pragma config FCMEN = ON       // Fail-Safe Clock Monitor Enable bit (FSCM timer enabled)
+#pragma config FEXTOSC  = OFF       // External Oscillator mode selection bits (Oscillator not enabled)
+#pragma config RSTOSC   = HFINT32   // Power-up default value for COSC bits (HFINTOSC with OSCFRQ= 32 MHz and CDIV = 1:1)
+#pragma config CLKOUTEN = OFF       // Clock Out Enable bit (CLKOUT function is disabled; i/o or oscillator function on OSC2)
+#pragma config CSWEN    = ON        // Clock Switch Enable bit (Writing to NOSC and NDIV is allowed)
+#pragma config FCMEN    = ON        // Fail-Safe Clock Monitor Enable bit (FSCM timer enabled)
 
 // CONFIG2
-#pragma config MCLRE = ON       // Master Clear Enable bit (MCLR pin is Master Clear function)
-#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
-#pragma config LPBOREN = OFF    // Low-Power BOR enable bit (ULPBOR disabled)
-#pragma config BOREN = ON       // Brown-out reset enable bits (Brown-out Reset Enabled, SBOREN bit is ignored)
-#pragma config BORV = LO        // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (VBOR) set to 1.9V on LF, and 2.45V on F Devices)
-#pragma config ZCD = OFF        // Zero-cross detect disable (Zero-cross detect circuit is disabled at POR.)
-#pragma config PPS1WAY = ON     // Peripheral Pin Select one-way control (The PPSLOCK bit can be cleared and set only once in software)
-#pragma config STVREN = ON      // Stack Overflow/Underflow Reset Enable bit (Stack Overflow or Underflow will cause a reset)
+#pragma config MCLRE    = ON        // Master Clear Enable bit (MCLR pin is Master Clear function)
+#pragma config PWRTE    = OFF       // Power-up Timer Enable bit (PWRT disabled)
+#pragma config LPBOREN  = OFF       // Low-Power BOR enable bit (ULPBOR disabled)
+#pragma config BOREN    = ON        // Brown-out reset enable bits (Brown-out Reset Enabled, SBOREN bit is ignored)
+#pragma config BORV     = LO        // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (VBOR) set to 1.9V on LF, and 2.45V on F Devices)
+#pragma config ZCD      = OFF       // Zero-cross detect disable (Zero-cross detect circuit is disabled at POR.)
+#pragma config PPS1WAY  = ON        // Peripheral Pin Select one-way control (The PPSLOCK bit can be cleared and set only once in software)
+#pragma config STVREN   = ON        // Stack Overflow/Underflow Reset Enable bit (Stack Overflow or Underflow will cause a reset)
 
 // CONFIG3
-#pragma config WDTCPS = WDTCPS_31// WDT Period Select bits (Divider ratio 1:65536; software control of WDTPS)
-#pragma config WDTE = OFF       // WDT operating mode (WDT Disabled, SWDTEN is ignored)
-#pragma config WDTCWS = WDTCWS_7// WDT Window Select bits (window always open (100%); software control; keyed access not required)
-#pragma config WDTCCS = SC      // WDT input clock selector (Software Control)
+#pragma config WDTCPS   = WDTCPS_31 // WDT Period Select bits (Divider ratio 1:65536; software control of WDTPS)
+#pragma config WDTE     = OFF       // WDT operating mode (WDT Disabled, SWDTEN is ignored)
+#pragma config WDTCWS   = WDTCWS_7  // WDT Window Select bits (window always open (100%); software control; keyed access not required)
+#pragma config WDTCCS   = SC        // WDT input clock selector (Software Control)
 
 // CONFIG4
-#pragma config WRT = OFF        // UserNVM self-write protection bits (Write protection off)
-#pragma config SCANE = available// Scanner Enable bit (Scanner module is available for use)
-#pragma config LVP = ON         // Low Voltage Programming Enable bit (Low Voltage programming enabled. MCLR/Vpp pin function is MCLR.)
+#pragma config WRT      = OFF       // UserNVM self-write protection bits (Write protection off)
+#pragma config SCANE    = available // Scanner Enable bit (Scanner module is available for use)
+#pragma config LVP      = ON        // Low Voltage Programming Enable bit (Low Voltage programming enabled. MCLR/Vpp pin function is MCLR.)
 
 // CONFIG5
-#pragma config CP = OFF         // UserNVM Program memory code protection bit (Program Memory code protection disabled)
-#pragma config CPD = OFF        // DataNVM code protection bit (Data EEPROM code protection disabled)
-
-// #pragma config statements should precede project file includes.
-// Use project enums instead of #define for ON and OFF.
+#pragma config CP       = OFF       // UserNVM Program memory code protection bit (Program Memory code protection disabled)
+#pragma config CPD      = OFF       // DataNVM code protection bit (Data EEPROM code protection disabled)
 
 #include <xc.h>
 #include <string.h>
 #include "image.h"
 #include "uart.h"
 
-#define comunicacaoLED LATAbits.LATA7
-#define robertsLED LATAbits.LATA6
+/*
+ * LEDs para indicar,respectivamente, que está havendo comunicação de dados entre o MCU e o computador e
+ * que o filtro de Roberts está sendo processado pelo MCU.
+ */
+#define comunicacaoLED  LATAbits.LATA7
+#define robertsLED      LATAbits.LATA6
 
+/*
+ *  Esta função configura alguns registradores do MCU pic16f18875, os pinos dos bloco A e B configurados como saída,
+ *  enquanto o bloco C tem o pino RC7 como entrada e os demais como saída.
+ */
 void ConfigurarPIC(void){
     TRISA = 0x00;
     TRISB = 0x00;
-    TRISC = 0b10000000;
+    TRISC = 0x80;
 }
 
+/*
+ * Esta função é chamada quando o comando GET é recebido na interface serial EUSART e envia os pixels referentes 
+ * à imagem de teste armazenada no arquivo image.h, serve para checar se a imagem foi armazenada corretamente.
+ */
 void EnviarImagem(void){
     short i,j;
     comunicacaoLED = 1;
-    for(i = 0; i < imgALTURA; i++){
-        for(j = 0; j < imgLARGURA; j++){
+    for(i = 1; i < imgALTURA - 1; i++){
+        for(j = 1; j < imgLARGURA - 1; j++){
             UART_Escrever_Pixel(pixels[i][j]);
         }
     }
     comunicacaoLED = 0;
 }
 
+/*
+ * Esta função é chamada quando o comando ROB é recebido na interface serial EUSART e serve para aplicar o 
+ * algoritmo de Roberts Cross para todos os pixels da imagem. Foi necessário adaptar o algoritmo, pois a imagem
+ * foi armazenada como const, pois, ao contrário, não seria possível armazenar os 3010 pixels referentes a esta.
+ * Assim sendo, para cada novo pixel calculado, este valor é imediatamente enviado para a interface serial, para que
+ * o computador, rodando o script em python possa receber esses valores e gerar a imagem resultante(bordas). 
+ * O laço que percorre a imagem inicia da primeira linha e primeira coluna e vai até a penúltima linha e penúltima 
+ * coluna, pois a imagem armazenada no arquivo image.h é o resultado da imagem original adicionada de um padding, uma
+ * borda feita apenas de pixels com intensidade 0 (preto) colocados ao redor de toda a imagem, 
+ * adicionando, assim, duas linhas e duas colunas a mais, que devem ser ignoradas do conjunto de pixels a se aplicar
+ * o filtro de Roberts.
+ */
 void RobertsCrossSerial(void){
     robertsLED = 1;
     short i,j;
     unsigned char novo_pixel;
-    for(i = 0; i < imgALTURA; i++){
-        for(j = 0; j < imgLARGURA; j++){
+    for(i = 1; i < imgALTURA - 1; i++){
+        for(j = 1; j < imgLARGURA - 1; j++){
             novo_pixel = RobertsCross(i,j);
             comunicacaoLED = 1;
             UART_Escrever_Pixel(novo_pixel);
@@ -84,8 +130,14 @@ void RobertsCrossSerial(void){
     robertsLED = 0;
 }
 
+/*
+ * A rotina principal configura os pinos de I/O e os registradores EUSART do MCU, acende o LED conectado ao pino
+ * RA7 para indicar funcionamento da rotina e, em seguida, coloca o MCU em modo de espera de um comando na interface
+ * serial EUSART. Caso o comando recebido seja GET, os pixels da imagem no arquivo image.h são enviados serialmente
+ * para o computador. Caso o comando o seja ROB, o MCU executa o filtro de Roberts Cross.
+ */
 int main(void) {
-    char entrada[6];
+    char entrada[4];
     
     ConfigurarPIC();
     UART_iniciar();  
--- uart.h
+++ uart.h
@@ -1,13 +1,23 @@
-/* 
- * File:   uart.h
- * Author: Tiago Dionizio
- *
- * Created on 11 de Novembro de 2018, 14:22
- */
+/*
+ *  Esta biblioteca pode ser usada para configurar a interface EUSART do MCU pic16f18875 e implementar rotinas de
+ *  leitura e escrita de dados serialmente.
+
+ *  Autores : Tiago Dionizio e Lucas Magalhães
+
+ *  Copyright (C) 2018 Tiago Siqueira Dionizio <tiagosdionizio@gmail.com>
+
+ *  Copyright (C) 2018 Lucas Magalhães  <lucasmag97@gmail.com> 
+
+ *  Data de Atualização : 20 de Novembro de 2018
+*/
 
 #ifndef UART_H
 #define	UART_H
 
+/*
+ * Esta função configura os registradores para usar a interface EUSART do MCU, configurando os pinos 
+ * RC6 como TX e RC7 como RX. O baudrate é fixo de 9600bps.
+ */
 void UART_iniciar(){
     ANSELCbits.ANSC7 = 0;   //Desativa função analógica no pino RC7
     ANSELCbits.ANSC6 = 0;   //Desativa função analógica no pino RC6
@@ -22,11 +32,17 @@ void UART_iniciar(){
     TX1STAbits.SYNC = 0;    //Configura EUSART para modo assíncrono
 }
 
+//Esta função serve para ler um simples byte na pino RC7 (RX).
 char UART_Ler(){
     while(!RCIF);
     return RCREG;
 }
 
+/*
+ * Esta função pode ler um conjunto de caracteres enviados serialmente para o pino RC7, serve para ler os comandos
+ * enviados pelo computador através do script em python. A regra é que cada comando contenha um caractere '$' 
+ * ao final, representando que o comando acabou.
+ */
 void UART_Ler_Texto(char* saida){
     short index = 0;
     char c;
@@ -38,16 +54,25 @@ void UART_Ler_Texto(char* saida){
     saida[index - 1] = '\0';
 }
 
+//Esta função serve para escrever um byte no pino RC6 (TX) do MCU.
 void UART_Escrever(char c){
     while(!TX1STAbits.TRMT);
     TXREG = c;
 }
 
+/*
+ * Esta função serve para enviar os pixels da imagem armazenada no arquivo image.h,
+ * bem como os novos valores de pixels calculados no filtro de Roberts Cross.
+ */
 void UART_Escrever_Pixel(unsigned char p){
     while(!TX1STAbits.TRMT);
     TXREG = p;
 }
 
+/*
+ * Esta função serve para escrever um conjunto de caracteres no pino RC6 (TX), não foi usada na aplicação,
+ * entretanto pode ser útil para outras aplicações.
+ */
 void UART_Escrever_Texto(char* texto){
     int index;
     for(index=0; texto[index]!='\0'; index++)
