87acd4bdb32ab194b1a1e45ea0e64fad92df9318
Lucas Magalhães <lucasmag97@gmail.com>
2018-11-24 01:36:22 -0300

    Corrige os comentários

--- ScriptSerial.py
+++ ScriptSerial.py
@@ -1,4 +1,4 @@
-/*
+'''
  *  Este script é responsável pela recepção dos bytes da imagem gerados pela MCU, logo após a aplicação do filtro. 
  *  A medida em que os bytes são recebidos, eles são guardados em um array. Quando a imagem estiver completa, o script gera
  *  um arquivo .PGM P5, contendo a imagem já pronta para teste.
@@ -10,16 +10,16 @@
  *  Copyright (C) 2018 Lucas Magalhães de Sousa <lucasmag97@gmail.com> 
 
  *  Data de Atualização : 21 de Novembro de 2018
-*/
+'''
 
 import serial
 from serial import Serial
 import array
 
-/*
+'''
  * Esta função define que a recepção será feita byte a byte, define o número total de pixels a serem recebidos e inicia a espera
  * dos bytes. Quando todos os bytes forem recebidos é retornado um bytearray contendo a imagem.
- */
+'''
 def recebe_pixels():
     nPixels = 0
     pixel = ser.read(1)
@@ -34,11 +34,11 @@ def recebe_pixels():
             pixels.append(ord(pixel))
     print("Transferência concluída!")
     return pixels
-
+'''
  * Quando o script é executado são definidas a porta de recepção e a taxa de transmissão. O comando ROB é enviado à MCU, para que se inicie
  * a execução do filtro e logo em seguida é iniciada a espera dos pixels convertidos. Depois a função cria um arquivo .PGM, escreve o 
  * cabeçalho e os bytes do bytearray, e por fim gera como saída a imagem .PGM com o filtro já aplicado.
- */
+'''
 if __name__ == "__main__":
     ser = serial.Serial(port = "/dev/ttyUSB0", baudrate = 9600, timeout = 0)
     ser.write(b'ROB$')
